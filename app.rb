051a940f64022883440c9acc37b61b7d367fb2b0
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-06-11 13:39:48 +0100

    final commit

---
+++ app.rb
@@ -0,0 +1,63 @@
+# my_number = 5
+# my_fisrt_string = 'Some string here'
+# a_slightly_longer_variable = 100.4
+#
+#
+#
+# puts my_number
+# puts my_fisrt_string
+# puts a_slightly_longer_variable.round
+
+# NUMBERS
+# age = 24.6
+# current_year = 2018.5
+# year_born = current_year - age
+#
+# age_in_ten_years = age + 10
+# double_my_age = age * 2
+# num_decades_old = age / 10 #need . if you want to get a .
+#
+# puts num_decades_old.to_i
+
+# STRINGS
+# my_first_name = "Ben"
+# my_last_name = "Tostevin"
+#
+# my_full_name = "#{my_first_name} #{my_last_name}"
+#
+# puts  my_full_name.length
+
+# SYMBOLS
+# my_name = "Ben" #string
+#
+# my_name = :Ben #symbol - for labelling
+#
+# puts my_name
+
+# ARRAYS
+# array_of_numbers = [1,2,3,4,5]
+# mixed_array =["Ben", :name, array_of_numbers, 24]
+#
+# p mixed_array.first
+
+# HASHES
+# first_hash = {
+#   name: "Ben",
+#   age: 24
+# }
+#
+#
+first_hash = {
+  :name => "Ben",
+  :age => 24
+}
+
+str = 'Some string'
+
+puts str.include?
+
+arr =[1,2,3]
+
+arr.reverse!
+
+puts arr
